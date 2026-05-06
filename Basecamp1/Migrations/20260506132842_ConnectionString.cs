008bb07a4216ef6a9554c2a3238812fa7a86d88b
be302_first <sirliyevaaysun93@gmail.com>
2026-05-06 17:31:08 +0400

    Complete Basecamp project

---
+++ Basecamp1/Migrations/20260506132842_ConnectionString.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basecamp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConnectionString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
