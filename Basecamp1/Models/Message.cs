adc97ffc07459010290bb7bd6f6c03e1a82234c0
be302_first <sirliyevaaysun93@gmail.com>
2026-05-14 10:28:34 +0400

    Add Attachment, Thread, Message models

---
+++ Basecamp1/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Basecamp1.Models;
+
+public class Message : BaseEntity
+{
+    public string Content { get; set; } = string.Empty;
+
+    public int ThreadId { get; set; }
+    public ForumThread? Thread { get; set; }  
+
+    public int UserId { get; set; }
+    public User? User { get; set; }
+}
