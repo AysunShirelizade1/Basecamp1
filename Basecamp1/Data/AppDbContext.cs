c6a542f58077c196d7dafbce3125eee96df1697a
be302_first <sirliyevaaysun93@gmail.com>
2026-05-14 10:29:12 +0400

    Update AppDbContext with new DbSets and relations

--- Basecamp1/Data/AppDbContext.cs
+++ Basecamp1/Data/AppDbContext.cs
@@ -1,15 +1,79 @@
-﻿namespace Basecamp1.Data;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Basecamp1.Models;
+
+namespace Basecamp1.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
     }
 
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Project> Projects { get; set; } = null!;
-    public DbSet<TaskItem> TaskItems { get; set; } = null!;
+    public DbSet<TaskItem> TaskItem { get; set; } = null!;  
+    public DbSet<Attachment> Attachments { get; set; } = null!;
+    public DbSet<ForumThread> Threads { get; set; } = null!;
+    public DbSet<Message> Messages { get; set; } = null!;
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>()
+            .HasOne(t => t.Project)
+            .WithMany(p => p.Tasks)
+            .HasForeignKey(t => t.ProjectId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+
+        modelBuilder.Entity<Attachment>()
+            .HasOne(a => a.Project)
+            .WithMany(p => p.Attachments)
+            .HasForeignKey(a => a.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Attachment>()
+            .HasOne(a => a.User)
+            .WithMany()
+            .HasForeignKey(a => a.UserId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<ForumThread>()
+            .HasOne(t => t.Project)
+            .WithMany(p => p.Threads)
+            .HasForeignKey(t => t.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<ForumThread>()
+            .HasOne(t => t.User)
+            .WithMany()
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Thread)
+            .WithMany(t => t.Messages)
+            .HasForeignKey(m => m.ThreadId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.User)
+            .WithMany()
+            .HasForeignKey(m => m.UserId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+
+        modelBuilder.Entity<Project>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.Projects)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Project>()
+            .HasOne(p => p.Admin)
+            .WithMany()
+            .HasForeignKey(p => p.AdminId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
