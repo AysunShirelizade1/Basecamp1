e5839fa7597107c8e5ef140a6fb1ee7854d3740d
be302_first <sirliyevaaysun93@gmail.com>
2026-05-15 17:09:16 +0400

    AttachmentUploadMetod

--- Basecamp1/Models/Attachment.cs
+++ Basecamp1/Models/Attachment.cs
@@ -3,12 +3,13 @@
 public class Attachment : BaseEntity
 {
     public string FileName { get; set; } = string.Empty;
-    public string FilePath { get; set; } = string.Empty;
-    public string Format { get; set; } = string.Empty; 
+    public string Format { get; set; } = string.Empty;
+    public byte[] FileData { get; set; } = Array.Empty<byte>();
+    public string ContentType { get; set; } = string.Empty;
 
     public int ProjectId { get; set; }
     public Project? Project { get; set; }
 
     public int UserId { get; set; }
-    public User? User { get; set; }
+    public User? User = null;
 }
